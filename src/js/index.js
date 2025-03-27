4847f388565ffaf30acf5d3a59c1c7c3412450e5
Yurii Ruban <yurii.ruban97@gmail.com>
2025-03-27 17:38:38 +0000

    feat: add contact module. Finish implementation

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,36 @@ import "../css/styles.css"
 
 import { generateMainPage } from "./mainpage.js"
 import { generateMenuPage } from "./menu.js"
+import { generateContactPage} from "./contact.js"
 
 const content = document.querySelector("#content");
-generateMenuPage(content);
+const homeBtn = document.querySelector(".home");
+const menuBtn = document.querySelector(".menu");
+const contactBtn = document.querySelector(".contact");
+
+function removeChildren(elem) {
+    Array.from(elem.children).map((item) => {
+        item.remove();
+    });
+}
+
+homeBtn.addEventListener("click", (ev) => {
+    removeChildren(content);
+    generateMainPage(content);
+});
+
+menuBtn.addEventListener("click", (ev) => {
+    removeChildren(content);
+    generateMenuPage(content);
+});
+
+contactBtn.addEventListener("click", (ev) => {
+    removeChildren(content);
+    generateContactPage(content);
+});
+
+
+generateMainPage(content);
 
 
 console.log('Im alive!');
